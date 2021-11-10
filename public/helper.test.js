742f05a1e0f4d7d709f4bea55c2a12bc740c40c5
Antonio Migotto <antoniomigotto@ufl.edu>
2021-11-10 13:23:56 -0500

    Added homepage, removed some redundant files

--- public/helper.test.js
+++ public/helper.test.js
@@ -1,69 +1,69 @@
 const {moneyCalc, powerCalc, exponentialIncrease, requiredPowerCalc} = require('./helper.js');
 
 test('money calculated should equal population * approval/100, test for success', () => {
-    expect(moneyCalc(300, 95)).toBe(285);
+	expect(moneyCalc(300, 95)).toBe(285);
 })
 
 test('money calculated should equal population * approval/100, test for truncating 1', () => {
-    expect(moneyCalc(333333, 92)).toBe(306666);
+	expect(moneyCalc(333333, 92)).toBe(306666);
 })
 
 test('money calculated should equal population * approval/100, test for truncating 2 (should not round)', () => {
-    expect(moneyCalc(333333, 93)).toBe(309999);
+	expect(moneyCalc(333333, 93)).toBe(309999);
 })
 
 test('power calculated should equal powerData * requiredPower * 1/100, test for success', () => {
-    expect(powerCalc(29,20000000 * 11)).toBe(63800000);
+	expect(powerCalc(29,20000000 * 11)).toBe(63800000);
 })
 
 test('power calculated should equal powerData * requiredPower * 1/100, test for rounding up', () => {
-    expect(powerCalc(33,53535)).toBe(17667);
+	expect(powerCalc(33,53535)).toBe(17667);
 })
 
 test('power calculated should equal powerData * requiredPower * 1/100, test for rounding down', () => {
-    expect(powerCalc(33,53531)).toBe(17665);
+	expect(powerCalc(33,53531)).toBe(17665);
 })
 
 test('people should increase by an increasing amount, 0 & 0 -> 1', () => {
-    let obj = {
-        reincrease: 0,
-        peopleIncrease: 0
-    }
-    expect(exponentialIncrease(obj)).toBe(1);
+	let obj = {
+		reincrease: 0,
+		peopleIncrease: 0
+	}
+	expect(exponentialIncrease(obj)).toBe(1);
 })
 
 test('people should increase by an increasing amount, 1 & 1 -> 3', () => {
-    let obj = {
-            reincrease: 1,
-            peopleIncrease: 1
-        }
-    expect(exponentialIncrease(obj)).toBe(3);
+	let obj = {
+		reincrease: 1,
+		peopleIncrease: 1
+	}
+	expect(exponentialIncrease(obj)).toBe(3);
 })
 
 test('people should increase by an increasing amount, 5 & 5 -> 11', () => {
-    let obj = {
-            reincrease: 5,
-            peopleIncrease: 5
-        }
-    expect(exponentialIncrease(obj)).toBe(11);
+	let obj = {
+		reincrease: 5,
+		peopleIncrease: 5
+	}
+	expect(exponentialIncrease(obj)).toBe(11);
 })
 
 test('people should increase by an increasing amount, 11 & 111 -> 123', () => {
-    let obj = {
-        reincrease: 11,
-        peopleIncrease: 111
-    }
-    expect(exponentialIncrease(obj)).toBe(123);
+	let obj = {
+		reincrease: 11,
+		peopleIncrease: 111
+	}
+	expect(exponentialIncrease(obj)).toBe(123);
 })
 
 test('required power is directly proportional to population, test for success', () => {
-    expect(requiredPowerCalc(1000)).toBe(893);
+	expect(requiredPowerCalc(1000)).toBe(893);
 })
 
 test('required power is directly proportional to population, test for rounding down', () => {
-    expect(requiredPowerCalc(12579)).toBe(11233);
+	expect(requiredPowerCalc(12579)).toBe(11233);
 })
 
 test('required power is directly proportional to population, test for rounding up', () => {
-    expect(requiredPowerCalc(13468)).toBe(12027);
+	expect(requiredPowerCalc(13468)).toBe(12027);
 })
--- public/script.js
+++ public/script.js
@@ -47,8 +47,8 @@ let el = document.getElementById('seconds-counter');
 let cancel = setInterval(incrementSeconds, 7500);
 
 let obj = {
-    peopleIncrease: 1,
-    reincrease: 1
+	peopleIncrease: 1,
+	reincrease: 1
 }
 
 let sumEnergy = 0;
@@ -56,247 +56,247 @@ let sumOfAllEnergy = 0;
 
 let i = 0;
 inputs.forEach(input => {
-    input.setAttribute('value', powerData[i++]);
+	input.setAttribute('value', powerData[i++]);
 });
 
 function updateAllStats() {
 
-    i = 0;
-    powers.forEach(power => {
-        powerData[i] = plantData[i++] * 100;
-    });
+	i = 0;
+	powers.forEach(power => {
+		powerData[i] = plantData[i++] * 100;
+	});
 
-    i = 0;
-    powers.forEach(power => {
-        power.textContent = (powerData[i++]).toLocaleString("en-US");
-    });
+	i = 0;
+	powers.forEach(power => {
+		power.textContent = (powerData[i++]).toLocaleString("en-US");
+	});
 
-    i = 0;
-    costs.forEach(cost => {
-        cost.firstElementChild.textContent = costData[i++];
-    });
+	i = 0;
+	costs.forEach(cost => {
+		cost.firstElementChild.textContent = costData[i++];
+	});
 
-    sumEnergy = 0;
-    sumOfAllEnergy = 0;
-    i = 0;
-    costs.forEach(cost => {
-        sumEnergy += powerData[i];
-        sumOfAllEnergy += powerData[i++];
-    });
-    sumEnergy *= seconds;
+	sumEnergy = 0;
+	sumOfAllEnergy = 0;
+	i = 0;
+	costs.forEach(cost => {
+		sumEnergy += powerData[i];
+		sumOfAllEnergy += powerData[i++];
+	});
+	sumEnergy *= seconds;
 
-    statData[1] -= sumEnergy;
-    statData[2] += exponentialIncrease(obj);
-    statData[3] += plantData[0] + plantData[1];
-    statData[4] -= statData[3] / 320 - 1;
-    if (statData[5] != 0 && statData[6] > statData[5]) {
-        statData[4] -= statData[6] / statData[5];
-    } else if (statData[5] != 0 && statData[6] < statData[5]) {
-        statData[4] += statData[5] / statData[6];
-    } else {
-        statData[4] -= statData[6] / 300;
-    }
-    if (statData[4] >= 100) {
-        statData[4] = 100;
-    }
-    statData[5] = sumOfAllEnergy;
-    statData[6] = requiredPowerCalc(statData[2]);
+	statData[1] -= sumEnergy;
+	statData[2] += exponentialIncrease(obj);
+	statData[3] += plantData[0] + plantData[1];
+	statData[4] -= statData[3] / 320 - 1;
+	if (statData[5] != 0 && statData[6] > statData[5]) {
+		statData[4] -= statData[6] / statData[5];
+	} else if (statData[5] != 0 && statData[6] < statData[5]) {
+		statData[4] += statData[5] / statData[6];
+	} else {
+		statData[4] -= statData[6] / 300;
+	}
+	if (statData[4] >= 100) {
+		statData[4] = 100;
+	}
+	statData[5] = sumOfAllEnergy;
+	statData[6] = requiredPowerCalc(statData[2]);
 
-    updateBudget();
+	updateBudget();
 
-    if (statData[4] <= 50 || statData[1] <= 0) {
-        modal.style.display = "block";
-    }
+	if (statData[4] <= 50 || statData[1] <= 0) {
+		modal.style.display = "block";
+	}
 
-    /*
-    i = 0;
-    outputs.forEach(output => {
-        output.textContent = powerData[i++];
-    });
-    */
+	/*
+	i = 0;
+	outputs.forEach(output => {
+		output.textContent = powerData[i++];
+	});
+	*/
 }
 
 
 function updateCostData() {
-    if (statData[0] == 2010) {
-        costData = [111, 82, 96, 107, 248, 124];
-    } else if (statData[0] == 2011) {
-        costData = [111, 83, 95, 104, 157, 71];
-    } else if (statData[0] == 2012) {
-        costData = [102, 75, 96, 116, 125, 72];
-    } else if (statData[0] == 2013) {
-        costData = [105, 74, 104, 116, 98, 70];
-    } else if (statData[0] == 2014) {
-        costData = [109, 74, 112, 116, 79, 59];
-    } else if (statData[0] == 2015) {
-        costData = [108, 65, 117, 100, 64, 55];
-    } else if (statData[0] == 2016) {
-        costData = [102, 63, 117, 98, 55, 47];
-    } else if (statData[0] == 2017) {
-        costData = [102, 60, 148, 97, 50, 45];
-    } else if (statData[0] == 2018) {
-        costData = [102, 58, 151, 91, 43, 42];
-    } else if (statData[0] == 2019) {
-        costData = [109, 56, 168, 91, 40, 41];
-    } else {
-        costData = [112, 58, 163, 80, 36, 40];
-    }
+	if (statData[0] == 2010) {
+		costData = [111, 82, 96, 107, 248, 124];
+	} else if (statData[0] == 2011) {
+		costData = [111, 83, 95, 104, 157, 71];
+	} else if (statData[0] == 2012) {
+		costData = [102, 75, 96, 116, 125, 72];
+	} else if (statData[0] == 2013) {
+		costData = [105, 74, 104, 116, 98, 70];
+	} else if (statData[0] == 2014) {
+		costData = [109, 74, 112, 116, 79, 59];
+	} else if (statData[0] == 2015) {
+		costData = [108, 65, 117, 100, 64, 55];
+	} else if (statData[0] == 2016) {
+		costData = [102, 63, 117, 98, 55, 47];
+	} else if (statData[0] == 2017) {
+		costData = [102, 60, 148, 97, 50, 45];
+	} else if (statData[0] == 2018) {
+		costData = [102, 58, 151, 91, 43, 42];
+	} else if (statData[0] == 2019) {
+		costData = [109, 56, 168, 91, 40, 41];
+	} else {
+		costData = [112, 58, 163, 80, 36, 40];
+	}
 }
 
 
 function updateBudget() {
-    statData[1] += moneyCalc(statData[2] , statData[4]);
+	statData[1] += moneyCalc(statData[2] , statData[4]);
 
-    i = 0;
-    stats.forEach(stat => {
-        if (i == 0) {
-            stat.lastElementChild.textContent = statData[i++];
-        }
-        else if (i == 5) {
-             stat.lastElementChild.textContent = Math.round(statData[i++]).toLocaleString("en-US");
-        } else {
-             stat.lastElementChild.textContent = statData[i++].toLocaleString("en-US"); // sebhastian.com/javascript-format-number-commas
+	i = 0;
+	stats.forEach(stat => {
+		if (i == 0) {
+			stat.lastElementChild.textContent = statData[i++];
+		}
+		else if (i == 5) {
+			stat.lastElementChild.textContent = Math.round(statData[i++]).toLocaleString("en-US");
+		} else {
+			stat.lastElementChild.textContent = statData[i++].toLocaleString("en-US"); // sebhastian.com/javascript-format-number-commas
 
-        }
-    });
+		}
+	});
 
-    //getmoney.firstElementChild.textContent = (statData[2] * (statData[4] / 100));
-    getmoney.firstElementChild.textContent = moneyCalc(statData[2],statData[4]).toLocaleString("en-US");
-    //getmoney.firstElementChild.textContent = Math.trunc(getmoney.firstElementChild.textContent).toLocaleString("en-US");
+	//getmoney.firstElementChild.textContent = (statData[2] * (statData[4] / 100));
+	getmoney.firstElementChild.textContent = moneyCalc(statData[2],statData[4]).toLocaleString("en-US");
+	//getmoney.firstElementChild.textContent = Math.trunc(getmoney.firstElementChild.textContent).toLocaleString("en-US");
 }
 
 function decrementBudget() {
-    statData[1] -= moneyCalc(statData[2] , statData[4]);
+	statData[1] -= moneyCalc(statData[2] , statData[4]);
 
-    i = 0;
-    stats.forEach(stat => {
-        if (i == 0) {
-            stat.lastElementChild.textContent = statData[i++];
-        }
-        else if (i == 5) {
-             stat.lastElementChild.textContent = Math.round(statData[i++]).toLocaleString("en-US");
-        } else {
-             stat.lastElementChild.textContent = statData[i++].toLocaleString("en-US"); // sebhastian.com/javascript-format-number-commas
+	i = 0;
+	stats.forEach(stat => {
+		if (i == 0) {
+			stat.lastElementChild.textContent = statData[i++];
+		}
+		else if (i == 5) {
+			stat.lastElementChild.textContent = Math.round(statData[i++]).toLocaleString("en-US");
+		} else {
+			stat.lastElementChild.textContent = statData[i++].toLocaleString("en-US"); // sebhastian.com/javascript-format-number-commas
 
-        }
-    });
+		}
+	});
 
-    //getmoney.firstElementChild.textContent = (statData[2] * (statData[4] / 100));
-    getmoney.firstElementChild.textContent = moneyCalc(statData[2],statData[4]).toLocaleString("en-US");
-    //getmoney.firstElementChild.textContent = Math.trunc(getmoney.firstElementChild.textContent).toLocaleString("en-US");
+	//getmoney.firstElementChild.textContent = (statData[2] * (statData[4] / 100));
+	getmoney.firstElementChild.textContent = moneyCalc(statData[2],statData[4]).toLocaleString("en-US");
+	//getmoney.firstElementChild.textContent = Math.trunc(getmoney.firstElementChild.textContent).toLocaleString("en-US");
 }
 
 function updateButtonOne() {
-    plantData[0]++;
-    button1owned.firstElementChild.textContent = plantData[0];
+	plantData[0]++;
+	button1owned.firstElementChild.textContent = plantData[0];
 }
 
 function destroyButtonOne() {
-    if (plantData[0] > 0) {
-        plantData[0]--;
-    }
-    decrementBudget();
-    button1owned.firstElementChild.textContent = plantData[0];
+	if (plantData[0] > 0) {
+		plantData[0]--;
+	}
+	decrementBudget();
+	button1owned.firstElementChild.textContent = plantData[0];
 }
 
 function updateButtonTwo() {
-    plantData[1]++;
-    button2owned.firstElementChild.textContent = plantData[1];
+	plantData[1]++;
+	button2owned.firstElementChild.textContent = plantData[1];
 }
 
 function destroyButtonTwo() {
-    if (plantData[1] > 0) {
-        plantData[1]--;
-    }
-    decrementBudget();
-    button2owned.firstElementChild.textContent = plantData[1];
+	if (plantData[1] > 0) {
+		plantData[1]--;
+	}
+	decrementBudget();
+	button2owned.firstElementChild.textContent = plantData[1];
 }
 
 function updateButtonThree() {
-    plantData[2]++;
-    button3owned.firstElementChild.textContent = plantData[2];
+	plantData[2]++;
+	button3owned.firstElementChild.textContent = plantData[2];
 }
 
 function destroyButtonThree() {
-    if (plantData[2] > 0) {
-        plantData[2]--;
-    }
-    decrementBudget();
-    button3owned.firstElementChild.textContent = plantData[2];
+	if (plantData[2] > 0) {
+		plantData[2]--;
+	}
+	decrementBudget();
+	button3owned.firstElementChild.textContent = plantData[2];
 }
 
 function updateButtonFour() {
-    plantData[3]++;
-    button4owned.firstElementChild.textContent = plantData[3];
+	plantData[3]++;
+	button4owned.firstElementChild.textContent = plantData[3];
 }
 
 function destroyButtonFour() {
-    if (plantData[3] > 0) {
-        plantData[3]--;
-    }
-    decrementBudget();
-    button4owned.firstElementChild.textContent = plantData[3];
+	if (plantData[3] > 0) {
+		plantData[3]--;
+	}
+	decrementBudget();
+	button4owned.firstElementChild.textContent = plantData[3];
 }
 
 function updateButtonFive() {
-    plantData[4]++;
-    button5owned.firstElementChild.textContent = plantData[4];
+	plantData[4]++;
+	button5owned.firstElementChild.textContent = plantData[4];
 }
 
 function destroyButtonFive() {
-    if (plantData[4] > 0) {
-        plantData[4]--;
-    }
-    decrementBudget();
-    button5owned.firstElementChild.textContent = plantData[4];
+	if (plantData[4] > 0) {
+		plantData[4]--;
+	}
+	decrementBudget();
+	button5owned.firstElementChild.textContent = plantData[4];
 }
 
 function updateButtonSix() {
-    plantData[5]++;
-    button6owned.firstElementChild.textContent = plantData[5];
+	plantData[5]++;
+	button6owned.firstElementChild.textContent = plantData[5];
 }
 
 function destroyButtonSix() {
-    if (plantData[5] > 0) {
-        plantData[5]--;
-    }
-    decrementBudget();
-    button6owned.firstElementChild.textContent = plantData[5];
+	if (plantData[5] > 0) {
+		plantData[5]--;
+	}
+	decrementBudget();
+	button6owned.firstElementChild.textContent = plantData[5];
 }
 
 function incrementSeconds() {
-    seconds += 1;
-    let month;
-    if (seconds % 12 == 1) {
-        month = "January";
-        statData[0]++;
-        updateCostData();
-    } else if (seconds % 12 == 2) {
-        month = "February";
-    } else if (seconds % 12 == 3) {
-        month = "March";
-    } else if (seconds % 12 == 4) {
-        month = "April";
-    } else if (seconds % 12 == 5) {
-        month = "May";
-    } else if (seconds % 12 == 6) {
-        month = "June";
-    } else if (seconds % 12 == 7) {
-        month = "July";
-    } else if (seconds % 12 == 8) {
-        month = "August";
-    } else if (seconds % 12 == 9) {
-        month = "September";
-    } else if (seconds % 12 == 10) {
-        month = "October";
-    } else if (seconds % 12 == 11) {
-        month = "November";
-    } else if (seconds % 12 == 0) {
-        month = "December";
-    }
-    el.innerText = month;
+	seconds += 1;
+	let month;
+	if (seconds % 12 == 1) {
+		month = "January";
+		statData[0]++;
+		updateCostData();
+	} else if (seconds % 12 == 2) {
+		month = "February";
+	} else if (seconds % 12 == 3) {
+		month = "March";
+	} else if (seconds % 12 == 4) {
+		month = "April";
+	} else if (seconds % 12 == 5) {
+		month = "May";
+	} else if (seconds % 12 == 6) {
+		month = "June";
+	} else if (seconds % 12 == 7) {
+		month = "July";
+	} else if (seconds % 12 == 8) {
+		month = "August";
+	} else if (seconds % 12 == 9) {
+		month = "September";
+	} else if (seconds % 12 == 10) {
+		month = "October";
+	} else if (seconds % 12 == 11) {
+		month = "November";
+	} else if (seconds % 12 == 0) {
+		month = "December";
+	}
+	el.innerText = month;
 
-    updateAllStats();
+	updateAllStats();
 }
 
 button1owned.firstElementChild.textContent = plantData[0];
